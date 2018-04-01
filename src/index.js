0f25ced4664daf52533601ecbbf392c122d9d4e1
santosh <webwerks@webwerks-santosh.local>
2018-04-01 16:41:49 +0530

    fixed css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory  } from 'react-router'
 import { Provider } from 'react-redux'
 
+import './assets/jointjs.css';
 import configureStore from './store/configureStore';
 import App from './containers/app';
 import Design from './components/design';
